a4e326e3b7b848bf1f05aead89026d320f96b896
Odim Kingsley <okekings@gmail.com>
2023-04-25 11:15:52 +0000

    function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,4 @@
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-
-  return students.map((student) => student.id);
+export default function getListStudentIds(arg) {
+  if (!Array.isArray(arg)) return [];
+  return arg.map((element) => element.id);
 }
-
